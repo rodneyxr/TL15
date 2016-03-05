a229fcb14daf723c0952d7c2fb37a614bc59c549
Rodney Rodriguez <rodneyxr@gmail.com>
2016-03-05 14:34:53 -0600

    Implement write token stream to file

--- src/core/Main.java
+++ src/core/Main.java
@@ -2,32 +2,61 @@ package core;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// validate the arguments
 		if (args.length != 1) {
 			System.err.println("Usage: prog <basename>.tl");
 			System.exit(1);
 		}
 
-		File file = new File(args[0]);
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			System.err.println("File not found: " + file.getName());
+		if (!args[0].endsWith(".tl")) {
+			System.err.println("Error: Source file must end with '.tl'");
 			System.exit(1);
 		}
 
-		Lexer lexer = new Lexer(scanner);
-		Token token = lexer.getToken();
-		while (token != null) {
-			System.out.println(token);
-			token = lexer.getToken();
+		// create the file
+		File sourceFile = new File(args[0]);
+
+		// create the scanner
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(sourceFile);
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + sourceFile.getName());
+			System.exit(1);
 		}
 
+		// create the lexer
+		Lexer lexer = new Lexer(scanner);
+
+		String tokFilename = sourceFile.getAbsolutePath().replaceAll("\\.tl$", ".tok");
+		File tokFile = new File(tokFilename);
+
+		// write the token stream to a file
+		try {
+			writeTokenStreamToFile(tokFile, lexer);
+		} catch (IOException e) {
+			System.err.println("Error writing to file: " + tokFile.getAbsolutePath());
+			System.exit(1);
+		}
+		System.out.println("File created: " + tokFile.getAbsolutePath());
+
+	}
+
+	private static void writeTokenStreamToFile(File file, Lexer lexer) throws FileNotFoundException {
+		PrintWriter w = new PrintWriter(file);
+		Token token = lexer.getToken();
+		while (token != null) {
+			w.println(token.toString());
+			token = lexer.getToken();
+		}
+		w.close();
 	}
 
 }
