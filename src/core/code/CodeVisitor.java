cfd1840d7f08d9e28523d1515eef33dc2e1775be
Rodney Rodriguez <rodneyxr@gmail.com>
2016-05-03 13:14:46 -0500

    Implement instruction list

--- src/core/code/CodeVisitor.java
+++ src/core/code/CodeVisitor.java
@@ -1,6 +1,8 @@
 package core.code;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import core.ast.Assignment;
 import core.ast.Declaration;
@@ -24,7 +26,8 @@ import core.parser.Symbol;
 public class CodeVisitor extends BaseVisitor {
 
 	HashMap<String, Symbol> table;
-	String code = "";
+	List<Instruction> instructions;
+
 	int tempReg = 0;
 	int ifCount = 0;
 
@@ -36,22 +39,30 @@ public class CodeVisitor extends BaseVisitor {
 
 	public CodeVisitor(HashMap<String, Symbol> symbolTable) {
 		this.table = symbolTable;
+		this.instructions = new ArrayList<>();
 	}
 
 	public String getCode() {
-		return code;
+		StringBuilder sb = new StringBuilder();
+		for (Instruction i : instructions) {
+			if (!i.isRaw() && !i.isLabel())
+				sb.append("    ");
+			sb.append(i);
+			sb.append('\n');
+		}
+		return sb.toString();
 	}
 
 	private void emit(String s) {
-		code += s + "\n";
+		instructions.add(new Instruction(s));
 	}
 
 	private void emit(Instruction instruction) {
-		emit("    " + instruction);
+		instructions.add(instruction);
 	}
 
 	private void emit(Label label) {
-		emit(label + ":");
+		instructions.add(new Instruction(label));
 	}
 
 	@Override
@@ -63,9 +74,11 @@ public class CodeVisitor extends BaseVisitor {
 		emit("main:");
 		emit("    li $fp, 0x7ffffffc");
 		emit("");
+		emit(new Label("StartBlock"));
+		emit("");
 		program.getDeclarations().accept(this);
 		program.getStatementSequence().accept(this);
-		emit("    # exit");
+		emit("# exit");
 		emit("    li $v0, 10");
 		emit("    syscall");
 	}
@@ -74,7 +87,7 @@ public class CodeVisitor extends BaseVisitor {
 	public void visit(Declaration declaration) throws ParserException {
 		declaration.getIdent().accept(this);
 		Register r1 = declaration.getIdent().reg;
-		emit("    # loadI 0 => r_" + declaration.getIdent().getVarName());
+		emit("# loadI 0 => r_" + declaration.getIdent().getVarName());
 		emit(new Instruction("li", $t0, 0));
 		emit(new Instruction("sw", $t0, r1));
 		emit("");
@@ -99,10 +112,10 @@ public class CodeVisitor extends BaseVisitor {
 		if (expression != null) {
 			expression.accept(this);
 			res = expression.reg;
-			emit("    # assignment => r_" + assignment.getIdentifier().getVarName());
+			emit("# assignment => r_" + assignment.getIdentifier().getVarName());
 			emit(new Instruction("lw", $t0, res));
 		} else {
-			emit("    # readInt => r_" + assignment.getIdentifier().getVarName());
+			emit("# readInt => r_" + assignment.getIdentifier().getVarName());
 			assignment.getReadInt().accept(this);
 			// readInt will store input into $v0
 			emit(new Instruction("move", $t0, $v0));
@@ -119,7 +132,7 @@ public class CodeVisitor extends BaseVisitor {
 		Label elseLabel = ifLabels[0];
 		Label endIfLabel = ifLabels[1];
 		Register res = ifStatement.getExpression().reg;
-		emit("    # IfStatement");
+		emit("# IfStatement");
 		emit(new Instruction("lw", $t0, res));
 		if (elseClause != null) {
 			emit(new Instruction("beqz", $t0, elseLabel));
@@ -153,7 +166,7 @@ public class CodeVisitor extends BaseVisitor {
 
 		whileStatement.getExpression().accept(this);
 		Register res = whileStatement.getExpression().reg;
-		emit("    # WhileStatement");
+		emit("# WhileStatement");
 		emit(new Instruction("lw", $t0, res));
 		emit(new Instruction("beqz", $t0, whileEnd));
 		emit("");
@@ -169,7 +182,7 @@ public class CodeVisitor extends BaseVisitor {
 	public void visit(WriteInt writeInt) throws ParserException {
 		writeInt.getExpression().accept(this);
 		Register r1 = writeInt.getExpression().reg;
-		emit("    # writeInt");
+		emit("# writeInt");
 		emit(new Instruction("li", $v0, 1));
 		emit(new Instruction("lw", $t1, r1));
 		emit(new Instruction("move", $a0, $t1));
@@ -204,42 +217,42 @@ public class CodeVisitor extends BaseVisitor {
 			Register r2 = right.reg;
 			switch (expression.getCompare().getText()) {
 			case Lexer.EQ:
-				emit("    # EQ");
+				emit("# EQ");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("seq", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.NE:
-				emit("    # NE");
+				emit("# NE");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("sne", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.LT:
-				emit("    # LT");
+				emit("# LT");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("slt", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.GT:
-				emit("    # GT");
+				emit("# GT");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("sgt", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.LE:
-				emit("    # LE");
+				emit("# LE");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("sle", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.GE:
-				emit("    # GE");
+				emit("# GE");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("sge", $t0, $t1, $t2));
@@ -265,14 +278,14 @@ public class CodeVisitor extends BaseVisitor {
 			Register r2 = right.reg;
 			switch (simpleExpression.getAdditive().getText()) {
 			case Lexer.ADD:
-				emit("    # add");
+				emit("# add");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("add", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.SUB:
-				emit("    # subtract");
+				emit("# subtract");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("sub", $t0, $t1, $t2));
@@ -298,21 +311,21 @@ public class CodeVisitor extends BaseVisitor {
 			Register r2 = right.reg;
 			switch (term.getMultiplicative()) {
 			case Lexer.MULT:
-				emit("    # multiply");
+				emit("# multiply");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("mul", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.DIV:
-				emit("    # divide");
+				emit("# divide");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("div", $t0, $t1, $t2));
 				emit(new Instruction("sw", $t0, res));
 				break;
 			case Lexer.MOD:
-				emit("    # modulus");
+				emit("# modulus");
 				emit(new Instruction("lw", $t1, r1));
 				emit(new Instruction("lw", $t2, r2));
 				emit(new Instruction("div", $t0, $t1, $t2));
@@ -343,7 +356,7 @@ public class CodeVisitor extends BaseVisitor {
 			Register r1 = Register.next();
 			factor.reg = r1;
 			int val = Integer.valueOf(num.getText());
-			emit(String.format("    # loadI %d => r%d", val, tempReg++));
+			emit(String.format("# loadI %d => r%d", val, tempReg++));
 			emit(new Instruction("li", $t0, val));
 			emit(new Instruction("sw", $t0, r1));
 			emit("");
@@ -358,7 +371,7 @@ public class CodeVisitor extends BaseVisitor {
 			int val = 0;
 			if (Boolean.valueOf(bool.getText()))
 				val = 1;
-			emit(String.format("    # loadI %d => r%d", val, tempReg++));
+			emit(String.format("# loadI %d => r%d", val, tempReg++));
 			emit(new Instruction("li", $t0, val));
 			emit(new Instruction("sw", $t0, r1));
 			emit("");
--- src/core/code/Instruction.java
+++ src/core/code/Instruction.java
@@ -3,6 +3,9 @@ package core.code;
 public class Instruction {
 
 	private String op;
+	private boolean isComment; // true if comment
+	private boolean isLabel; // true if label
+	private boolean isRaw; // true if no instructions match
 
 	private Register r1;
 	private Register r2;
@@ -13,11 +16,17 @@ public class Instruction {
 	private Label label;
 
 	public Instruction(String op) {
-		if (!op.equals("syscall")) {
-			System.err.println("Instruction.java: Invalid MIPS instruction");
-			System.exit(1);
+		if (op.matches("^\\s*#.*")) {
+			isComment = true;
+		} else if (!op.equals("syscall")) {
+			isRaw = true;
 		}
 		this.op = op;
+	}
+
+	public Instruction(Label label) {
+		this.label = label;
+		this.isLabel = true;
 	}
 
 	public Instruction(String op, Label label) {
@@ -50,18 +59,32 @@ public class Instruction {
 		this.r3 = r3;
 	}
 
+	public boolean isRaw() {
+		return isRaw;
+	}
+
+	public boolean isLabel() {
+		return isLabel;
+	}
+
 	@Override
 	public String toString() {
-		if (op.equals("syscall"))
+		if (isLabel)
+			return String.format("%s:", label);
+
+		if (op.equals("syscall") || isComment || isRaw)
 			return op;
+
 		if (value != null)
 			return String.format("%s %s, %d", op, r1, value);
+
 		if (label != null) {
 			if (r1 != null)
 				return String.format("%s %s, %s", op, r1, label);
 			else
 				return String.format("%s %s", op, label);
 		}
+
 		if (r3 == null)
 			return String.format("%s %s, %s", op, r1, r2);
 		return String.format("%s %s, %s, %s", op, r1, r2, r3);
